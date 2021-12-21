6c4e270dc71a2f5da83c1137078564a968881e88
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-21 16:41:08 +0900

    commit

---
+++ src/main/java/myspringboot/reactive/mongo/filter/LogFilter.java
@@ -0,0 +1,29 @@
+package myspringboot.reactive.mongo.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LogFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        long startTime = System.currentTimeMillis();
+        String path = exchange.getRequest().getURI().getPath();
+        log.info("Serving '{}'", path);
+
+        return chain.filter(exchange).doAfterTerminate(() -> {
+                    exchange.getResponse().getHeaders().entrySet().forEach(e ->
+                            log.info("Response Headers '{}' : '{}'",e.getKey(),e.getValue()));
+                    log.info("Served '{}' as {} in {} ms",
+                            path,
+                            exchange.getResponse().getStatusCode(),
+                            System.currentTimeMillis() - startTime);
+                }
+        );
+    }
+}
