2e72de180b711444f1af5f1fe76187d155fe8f82
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-21 16:05:23 +0900

    commit

--- src/main/java/myspringboot/reactive/mongo/handler/ProductHandler.java
+++ src/main/java/myspringboot/reactive/mongo/handler/ProductHandler.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import myspringboot.reactive.mongo.dto.ProductDto;
 import myspringboot.reactive.mongo.repository.ProductRepository;
 import myspringboot.reactive.mongo.utils.AppUtils;
+import org.springframework.data.domain.Range;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -41,5 +42,19 @@ public class ProductHandler
                                                             .body(fromValue(productDto))
                          )  // Mono<ServerResponse>
                          .switchIfEmpty(response404);
+    }
+
+    public Mono<ServerResponse> getProductInRange(ServerRequest request)
+    {
+        double min = Double.parseDouble(request.queryParam("min").orElseGet(() -> Double.toString(Double.MIN_VALUE)));
+        System.out.println("min = " + min);
+        double max = Double.parseDouble(request.queryParam("max").orElseGet(() -> Double.toString(Double.MAX_VALUE)));
+        System.out.println("max = " + max);
+
+        Flux<ProductDto> productDtoFlux = repository.findByPriceBetween(Range.closed(min, max)).map(AppUtils::entityToDto);
+
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                                  .body(productDtoFlux, productDto.class)
+                                  .switchIfEmpty(response404);
     }
 }
