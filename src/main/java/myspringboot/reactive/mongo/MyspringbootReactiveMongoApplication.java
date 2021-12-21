169541fe311b24757bf6102b2b714f5968faad3b
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-12-21 11:24:18 +0900

    first commit

---
+++ src/main/java/myspringboot/reactive/mongo/MyspringbootReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package myspringboot.reactive.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbootReactiveMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringbootReactiveMongoApplication.class, args);
+	}
+
+}
